365c7c27e876e27c8364383827c8e231a47ad238
Dallas Hutchinson <62364481+dallas-hutch@users.noreply.github.com>
2022-09-27 11:38:35 -0400

    Create nn.py

---
+++ nn.py
@@ -0,0 +1,15 @@
+"""
+Complete the functions and the other sections below to build a complete model of a simple neural network 
+(equivalent to Logistic Regression). 
+The incomplete parts are marked and you need to fill them up to finish this assignment
+
+DATA: handwritten digits classified into two classes Even (0) and Odd (1)
+"""
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import ndimage
+from sklearn import datasets
